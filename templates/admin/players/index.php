19a9b87013ddf164c4f89aaab11d2c9abdbb57e4
labirinto <delevkoo@gmail.com>
2019-10-07 13:34:14 +0200

    players list add

--- templates/admin/players/index.php
+++ templates/admin/players/index.php
@@ -6,12 +6,67 @@
 
 <?php 
 
-$data = query("SELECT P.id, CONCAT(P.lastName, ' ', P.firstName) FROM player P 
+$data = query("SELECT P.id, P.lastName, P.firstName, P.photo
+			FROM player P 
 			WHERE P.id NOT IN(-1,-2) ORDER BY 2");
-    
-for($i = 0; $i<count($data); $i++)
+
+if(count($data) > 0)
 {
-	$player = $data[$i][1];
-    print("$player</br>\n");
+	generateHeader();
+
+	for($i = 0; $i<count($data); $i++)
+	{
+		$id = $data[$i][0];
+		$fName = $data[$i][1];
+		$lName = $data[$i][2];
+		$img = $data[$i][3];
+
+		printPlayer($id, $fName, $lName, $img);
+		//print("<a href=\"lobby.php?id=".$id."\">".$fName." ".$lName."</a></br>\n");
+	}
 }
 
+
+function generateHeader()
+{ ?>
+     <header>
+		<i class="fas fa-user"></i>
+		<h1 class="players_list_header">Гравці</h1>
+	</header>
+
+	<section>
+		<div class="players_list_search_field">
+			<form class="centered_search_div" action="#">
+			  <input id="myInput" onkeyup="myFunction()" type="text" placeholder="Пошук.." name="search">
+			</form>
+		</div>
+		<div class="players_list_box">
+			<ul class="players_u-list_item" id="myUL">
+<?php }
+
+function printPlayer($playerID, $fName, $lName, $img)
+{ ?>
+
+				<li>
+					<a href="lobby.php?id=<?=$playerID?>">
+					<div class="players_list_item_box">
+						<figure>
+							<img class="players_list_item_photo" alt="фото_гравця" src="<?=PLAYER_IMG.$img?>">
+						</figure>
+						<div class="players_list_item_details">
+							<div class="players_list_item_name">
+								<h4 class="players_list_item_name">
+									<?=$fName?> <span class="surname"><?=$lName?></span>
+								</h4>
+							</div>
+								<p class="players_list_item_location">
+									Львів, Україна
+								</p>
+						</div>
+					</div>
+					</a>
+				</li>
+
+<?php
+}
+?>
---
+++ templates/admin/players/lobby.php
@@ -0,0 +1,78 @@
+
+		<div class="player_profile_section01">
+			<div class="player_profile_photo">
+				<figure>
+					<img class="player_profile_img" src="<?=PLAYER_IMG.$img?>" alt="Фото гравця">
+				</figure>
+			</div>
+			<div class="player_profile_personalInfo">
+				<table>
+					<th colspan="2">
+						<div class="player_profile_playerName">
+							<i class="fas fa-user"></i>
+							<div class="players_list_header"><?=$fName?> <span class="player_profile_surname"><?=$lName?></span></div>
+						</div>
+					</th>
+					<tr>
+						<td>
+							<div class="player_profile_country">
+								<div class="player_profile_tableHeader">країна</div>
+								<div class="player_profile_tabelMean">Україна</div>
+							</div>
+						</td>
+						<td>
+							<div class="player_profile_city">
+								<div class="player_profile_tableHeader">місто</div>
+								<div class="player_profile_tabelMean">Львів</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="player_profile_age">
+								<div class="player_profile_tableHeader">дата народження</div>
+								<div class="player_profile_tabelMean">14.08.1997</div>
+							</div>
+						</td>
+						<td>
+							<div class="player_profile_maxBreak">
+								<div class="player_profile_tableHeader">найвищий турнірний<br>брейк</div>
+								<div class="player_profile_tabelMean max_break">128</div>
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div> <br>
+
+			<!-- SECTION 02 (CIRCLES) -->
+
+		<div class="player_profile_section02">
+			<a class="circle_hover" href="#">
+			<div class="player_profile_tournamentCircle player_profile_circle">
+				<div class="little_circle">37</div>
+				<i class="fas fa-trophy"></i>
+			</div>
+			</a>
+			<a class="circle_hover" href="#">
+			<div class="player_profile_tournamentBreakCircle player_profile_circle">
+				<div class="little_circle">19</div>
+				<i class="fas fa-trophy break_icon"></i> <br>
+				<span class="circle_break">брейки</span>
+			</div>
+			</a>
+			<a class="circle_hover" href="#" >
+			<div class="player_profile_sparingCircle player_profile_circle">
+				<div class="little_circle">80</div>
+				<i class="fas fa-user-friends"></i>
+			</div>
+			</a>
+			<a class="circle_hover" href="#">
+			<div class="player_profile_sparingBreakCircle player_profile_circle">
+				<div class="little_circle">44</div>
+				<i class="fas fa-user-friends break_icon"></i> <br>
+				<span class="circle_break">брейки</span>
+			</div>
+			</a>
+		</div>
+
