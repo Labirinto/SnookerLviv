a42f05a6f00df73b065568daf85a0903afe101d3
labirinto <delevkoo@gmail.com>
2019-10-17 17:18:29 +0200

    proper putPlayerInNextMatch for tournaments

--- database/tournamentProcedures.sql
+++ database/tournamentProcedures.sql
@@ -27,7 +27,7 @@ BEGIN
 	WHERE PT.tournamentID = tournament AND PT.seed = seed;
 
 	IF matchID != -1 THEN
-		CALL putPlayerIntoNextMatch(matchID, playerID, 0);
+		CALL putPlayerIntoMatch(matchID, playerID, 0);
 	END IF;
 END;
 
