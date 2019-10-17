0713d17a7ade2a8e4cd7eb87273b72952fcc7203
labirinto <delevkoo@gmail.com>
2019-10-17 10:55:10 +0200

    add functions for dymanic player search

--- public/js/player_search.js
+++ public/js/player_search.js
@@ -1,20 +1,51 @@
-function myFunction()
+function search()
 {
-	var input, filter, ul, li, a, i, txtValue;
-	input = document.getElementById('myInput');
-	filter = input.value.toUpperCase();
-	ul = document.getElementById("myUL");
-	li = ul.getElementsByTagName('li');
+    var input, filter, table, tr, td, i, txtValue, ul, li, a;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
 
-	for (i = 0; i < li.length; i++)
-	{
-		a = li[i].getElementsByTagName("a")[0];
-		txtValue = a.textContent || a.innerText;
-		if (txtValue.toUpperCase().indexOf(filter) > -1){
-			li[i].style.display = "";
-		} else {
-			li[i].style.display = "none";
-		}
-	}
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        
+        if(td) {
+            txtValue = td.textContent || td.innerText;
+            
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        } 
+    }
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "flex";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.className += " active";
 }
 
