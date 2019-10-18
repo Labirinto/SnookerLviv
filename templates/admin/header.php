3c4600f7ee773befee8b770d754561fe16557375
labirinto <delevkoo@gmail.com>
2019-10-18 13:18:29 +0200

    modify styles depending on new structure

--- templates/admin/header.php
+++ templates/admin/header.php
@@ -11,10 +11,10 @@
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/matches-list.css"> 
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/lobby_list_styles.css"> 
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/match_lobby_style.css"> 
-    <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/tournament_list_style.css"> 
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/player_profile_style.css"> 
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/participants.css"> 
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/breaks.css"> 
+    <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/tournament_list.css"> 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="icon" type="image/x-icon" href="<?=PATH_H?>img/balls01.png">
