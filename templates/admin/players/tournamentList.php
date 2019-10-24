d855262a27048e7a832cf155251ecdd1f9d6b844
labirinto <delevkoo@gmail.com>
2019-10-24 23:17:03 +0200

    refactor player tournaments accrdingly with styles

--- templates/admin/players/tournamentList.php
+++ templates/admin/players/tournamentList.php
@@ -1,8 +1,9 @@
 <?php
 
-	$query = "select tournamentName,tournamentID,clubName,place,points
-			from playerTournamentView where playerID=?
-			order by 5 DESC, 4;";
+	$query = "SELECT tournamentName,tournamentID,clubName,place,points
+			FROM playerTournamentView
+			WHERE playerID=? AND place IS NOT NULL
+			ORDER BY 5 DESC, 4;";
 
 	$data = query($query, $playerID);
 	$data_count = count($data);
@@ -15,19 +16,19 @@
         $name = $data[$i][0]; $id = $data[$i][1];
 		$clubName = $data[$i][2]; $place = $data[$i][3];
 		$pts = $data[$i][4]; $isLast = ($i+1==$data_count);
-	 	displayTournament($i+1,$id,$name,$clubName,$isLast,$place);
+	 	displayTournament($i+1,$id,$name,$clubName,$isLast,$place,$pts);
 	}
 
 	listFooter();
 
 
-function displayTournament($i, $id, $name, $clubName, $isLast, $place)
+function displayTournament($i, $id, $name, $clubName, $isLast, $place,$pts)
 {
     $e_o = ($i%2) ? "odd" : "even";
 ?>
             <tr onclick="openTournamentLobby(<?=$id?>);"
             class="tbody_<?=$e_o?> pointer">
-                <td class="calendar_table_points <?=$e_o?>_num<?=($isLast)?" radius_bl":""?>">
+                <td class="player_tournaments_table_points <?=$e_o?>_num<?=($isLast)?" radius_bl":""?>">
                     <?=$i?>
                 </td>
                 <td>
@@ -36,8 +37,14 @@ function displayTournament($i, $id, $name, $clubName, $isLast, $place)
                 <td>
                     <?=$clubName?>
                 </td>
-                <td class="<?=($isLast)?"radius_br":""?>">
+                <td>
+                    ДАТА
+                </td>
+                <td>
                     <?=$place?>
+                </td>
+                <td class="<?=($isLast)?"radius_br":""?>">
+                    <?=$pts?>
                 </td>
             </tr>
 <?php
@@ -47,35 +54,43 @@ function displayTournament($i, $id, $name, $clubName, $isLast, $place)
 function listHeader()
 { ?>
     <div class="sub-container">
-        <div class="calendar_header">
-            <h1 class="calendar_sign">Турніри</h1>
+        <div class="player_tournaments_header">
+            <h1 class="player_tournaments_sign">Турніри</h1>
         </div>
-		<div class="calendar_table_container">
-		<table class="calendar_table">
+		<div class="player_tournaments_table_container">
+		<table class="player_tournaments_table">
 			<colgroup>
 				<col class="col-1">
 				<col class="col-2">
 				<col class="col-3">
 				<col class="col-4">
+				<col class="col-5">
+				<col class="col-6">
 			</colgroup>
-			<thead class="calendar_table_thead">
+			<thead class="player_tournaments_table_thead">
 				<tr>
 					<th>#</th>
 					<th>
-						<img class="thead_icon" alt="trophy_image" src="<?=PATH_H?>img/web/trophy.png">
+						<img class="thead_icon" alt="trophy_img" src="<?=PATH_H?>img/web/trophy.png">
 						<span>Турнір</span>
 					</th>
 					<th>
-						<img class="thead_icon" alt="trophy_image" width="9" src="<?=PATH_H?>img/web/location.png">
+						<img class="thead_icon" alt="location_img" width="9" src="<?=PATH_H?>img/web/location.png">
 						<span>Клуб</span>
 					</th>
 					<th>
-						<img class="thead_icon" alt="trophy_image" src="<?=PATH_H?>img/web/calendar.png">
+						<img class="thead_icon" alt="calendar_img" src="<?=PATH_H?>img/web/calendar.png">
 						<span>Дата</span>
+					</th>
+					<th>
+						Місце
+					</th>
+					<th>
+						Очки
 					</th>
 				</tr>
 			</thead>
-			<tbody class="calendar_table_tbody">
+			<tbody class="player_tournaments_table_tbody">
 <?php
 }
 function listFooter()
