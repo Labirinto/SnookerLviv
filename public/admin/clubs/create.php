e4bf3cb83e9787d2bf798dabcfd384a3855ce715
labirinto <delevkoo@gmail.com>
2019-10-26 18:07:42 +0200

    add club photo upload

--- public/admin/clubs/create.php
+++ public/admin/clubs/create.php
@@ -19,11 +19,36 @@ else if($_SERVER["REQUEST_METHOD"] == "POST")
 		adminApology(INPUT_ERROR, "All fields required");
 		exit;
 	}
+
+	if( !$_FILES["photo"]["size"] )
+    {
+        $photo = "default.png";
+    }
+    else
+    {
+        $photo = $name . ".jpg";
+        $filepath = HOME_DIR . "public/img/club/" . $photo;
+
+        if( !getimagesize($_FILES["photo"]["tmp_name"]) )
+        {
+            adminApology(INPUT_ERROR, "Upload actual photo");
+            exit;
+        }
+        if( !move_uploaded_file($_FILES["photo"]["tmp_name"], $filepath) )
+        {
+            adminApology(INPUT_ERROR, "Photo error"." ".$filepath." ".$_FILES["photo"]["name"]);
+            exit;
+        }
+    }
+
+
+
+
 	
-	$query = "INSERT INTO club(name, country, city, nrOfTables) 
-			VALUES(?,?,?,?)";
+	$query = "INSERT INTO club(name, country, city, nrOfTables, photo) 
+			VALUES(?,?,?,?,?)";
 	
-	query($query, $name, $country, $city, $tables);
+	query($query, $name, $country, $city, $tables, $photo);
 	redirect("");
 }
 
