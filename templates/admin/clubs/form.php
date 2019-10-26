288e4e8ada8b634b4fd023535fe7fac61cac951c
labirinto <delevkoo@gmail.com>
2019-10-26 18:08:34 +0200

    add club photo upload

--- templates/admin/clubs/form.php
+++ templates/admin/clubs/form.php
@@ -1,13 +1,41 @@
-<fieldset>
-	<legend>Club:</legend>
-	<form action="create.php" method="post">
-		Name: <input autofocus name="name" type="text" class="black_text"/>
-		</br></br>
-		Country: <input name="country" type="text" class="black_text"/></br>
-		City: <input name="city" type="text" class="black_text"/>
-		</br></br>
-		Number of snooker tables:</br>
-		<input name="tables" type="number" class="black_text"/> </br>
-		<button type="submit" class="black_text">Create</button>
-	</form>
-</fieldset>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
+    </script>
+
+
+	<div class="sub-container">
+		Дані клубу:
+		<form action="create.php" method="post" enctype="multipart/form-data">
+            <div class="avatar-upload">
+                <div class="avatar-preview">
+                    <div id="imgPreview"
+					style="background-image: url(http://localhost/~levko/img/club/default.png);">
+					</div>
+                </div>
+                <div class="avatar-edit">
+                    <input type='file' id="imgUpload" accept=".png, .jpg, .jpeg" name="photo"/>
+                    <label for="imgUpload">
+                        <i class="fas fa-upload"></i>
+                        Завантажити
+                    </label>
+                </div>
+            </div>
+			
+			Назва: <input autofocus name="name" type="text"/>
+			</br></br>
+			Країна: <input name="country" type="text"/>
+			</br>
+			Місто: <input name="city" type="text"/>
+			</br></br>
+			Кількість столів:
+			</br>
+			<input name="tables" type="number"/>
+			</br></br>
+			<button type="submit">
+				Створити
+			</button>
+        </form>
+	</div>
+
+    <script type="text/javascript" src="/~levko/js/img_upload.js">
+    </script>
+
