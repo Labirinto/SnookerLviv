a2f85c71ecc1238759decf0663135a8622ce93be
labirinto <delevkoo@gmail.com>
2019-10-26 18:07:22 +0200

    change class name in js function

--- public/js/img_upload.js
+++ public/js/img_upload.js
@@ -2,13 +2,13 @@ function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function(e) {
-            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
-            $('#imagePreview').hide();
-            $('#imagePreview').fadeIn(650);
+            $('#imgPreview').css('background-image', 'url('+e.target.result +')');
+            $('#imgPreview').hide();
+            $('#imgPreview').fadeIn(650);
         }
         reader.readAsDataURL(input.files[0]);
     }
 }
-$("#imageUpload").change(function() {
+$("#imgUpload").change(function() {
     readURL(this);
 });
