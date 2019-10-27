b2670104b958fff23bf97a374b48b4795529a872
labirinto <delevkoo@gmail.com>
2019-10-27 18:15:10 +0100

    change header navigation accordingly to styles

--- templates/admin/header.php
+++ templates/admin/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title><?=htmlspecialchars($title)?></title>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/normalize.css">
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/navigation.css">
     <link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/bracket.css">
@@ -25,7 +25,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="icon" type="image/x-icon" href="<?=PATH_H?>img/balls01.png">
+	
 	<link href="https://fonts.googleapis.com/css?family=Exo+2&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather:400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
 	<script type="text/javascript" src="/~levko/js/functions.js">
@@ -35,28 +37,41 @@
 
 <body>
      <header>
-        <div class="container">
-            <div class="logologin clearfix">
-                <img src="<?=PATH_H?>img/balls01.png" alt="SnookerLviv">
-                <form action="<?=PATH_H?>logout.php" class="login" method="post">
-                    <input type="submit" value="Logout">
-                </form>
-                <form action="<?=PATH_H?>playerHome.php" class="login" method="post">
-                    <input type="submit" value="Home">
-                </form>
-                <nav class="navigation">
-                    <ul>
-                        <li><a href="<?=PATH_H?>admin/tournaments">Турніри</a></li>
-                        <li><a href="<?=PATH_H?>admin/players">Гравці</a></li>
-                        <li><a href="<?=PATH_H?>admin/clubs">Клуби</a></li>
-                        <li><a href="<?=PATH_H?>admin/leagues">Ліги</a></li>
-                        <li><a href="<?=PATH_H?>admin/rankings">Рейтинги</a></li>
-                        <li><a href="<?=PATH_H?>admin/matches">Матчі</a></li>
-                    </ul>
-                </nav>
-            </div>
+        <!-- LOGO -->
+        <div class="logo_box">
+        <img class="logo_img" src="<?=PATH_H?>img/sl_logo.png" alt="SnookerLviv">
+        <span class="logo_text">billiard hub</span>
         </div>
-     </header>
+
+
+        <!-- NAV MENU -->
+         <nav class="navigation" id="myTopnav">
+
+                <a href="<?=PATH_H?>admin/tournaments">Турніри</a>
+                <a href="<?=PATH_H?>admin/players">Гравці</a>
+                <a href="<?=PATH_H?>admin/clubs">Клуби</a>
+                <a href="<?=PATH_H?>admin/leagues">Ліги</a>
+                <a href="<?=PATH_H?>admin/rankings">Рейтинги</a>
+                <a href="<?=PATH_H?>admin/matches">Матчі</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                 <i class="fa fa-bars"></i>
+                 </a>
+
+        </nav>
+
+        <!-- BUTTONS -->
+        <div class="header_buttons">
+        <form action="<?=PATH_H?>logout.php" class="login" method="post">
+            <i class="fas fa-sign-out-alt"></i>
+			<input id="childOne" type="submit" value="Вийти">
+        </form>
+        <form action="<?=PATH_H?>playerHome.php" class="login" method="post">
+            <i class="fas fa-home"></i>
+			<input type="submit" value="Дім">
+        </form>
+        </div>
+
+	</header>
 
 	<div class="container">
 
