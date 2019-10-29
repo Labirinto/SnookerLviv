dfdc2b6aaa47f6031751e12b970bd0d806cab32b
labirinto <delevkoo@gmail.com>
2019-10-29 19:13:12 +0100

    create function to highlight header navigation button;

---
+++ public/js/header_highlight.js
@@ -0,0 +1,16 @@
+function get_location(_href) {
+	var arr = _href.split('/');
+	return (arr < 2) ? arr[0] : arr[arr.length-2];
+}
+
+function set_highlight()
+{
+	_href = document.location.href ? document.location.href : document.location;
+	
+	var _location = get_location(_href);
+	document.getElementById(_location).className = "active";
+}
+
+window.onload=function() {
+	set_highlight();
+}
