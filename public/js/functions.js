1820210dc03d339fcb386a7bdf58756433dfffb9
labirinto <delevkoo@gmail.com>
2019-10-31 03:04:20 +0100

    add openClub function

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,11 +1,14 @@
 function openPlayerLobby(ID) {
-	window.location.href = ('/~levko/admin/players/lobby.php?id='+ID);
+	window.location.href = ('/~levko/players/lobby.php?id='+ID);
 }
 function openMatchLobby(ID) {
-    window.location.href = ('/~levko/admin/tournaments/matchLobby.php?id=' + ID);
+    window.location.href = ('/~levko/tournaments/matchLobby.php?id=' + ID);
 }
 function openTournamentLobby(ID) {
-	window.location.href = ('/~levko/admin/tournaments/lobby.php?id=' + ID);
+	window.location.href = ('/~levko/tournaments/lobby.php?id=' + ID);
+}
+function openRating(ID) {
+	window.location.href = ('/~levko/rankings/ranking.php?id=' + ID);
 }
 function openYoutube(event, youtube) {
     event.stopPropagation();
