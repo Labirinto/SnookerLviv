b07cca23ec2080ae0f71e2553a4ecd13121086a6
labirinto <delevkoo@gmail.com>
2019-10-31 16:33:53 +0100

    change login form display + add styles

--- templates/loginForm.php
+++ templates/loginForm.php
@@ -1,9 +1,23 @@
 	
-	</br></br>
-	<form action="<?=PATH_H?>login.php" method="post">
-		Ім'я користувача: <input autofocus name="username" type="text"/>
-		</br>
-		Пароль:  <input name="password" type="password"/>
-		</br>
-		<button type="submit">Увійти</button>
-	</form>
+<link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/login_form.css"> 
+		<div class="margin-b_30"></div>
+		<div class="login_box">
+            <div class="login_img">
+                <img src="img/sl_logo.png" alt="BilliardHub Logo">
+            </div>
+            <div class="login_header">
+                <span>Вхід</span>
+            </div>
+            <form class="login_form" action="<?=PATH_H?>login.php" method="post">
+              <input type="text" name="username" placeholder="Ім'я користувача">
+              <input type="password" name="password" placeholder="Пароль">
+              <button>Увійти</button>
+            </form>
+            
+			<a href="<?=PATH_H?>register.php">
+				<div class="login_register">
+					<span>Зареєструватись</span>
+				</div>
+			</a>
+        </div>
+
