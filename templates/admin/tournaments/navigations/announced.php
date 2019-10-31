d9383594fde1b99e5445524c83303e6b3b74d388
labirinto <delevkoo@gmail.com>
2019-10-31 02:55:10 +0100

    move tournaments to default folder

--- templates/admin/tournaments/navigations/announced.php
+++
@@ -1,7 +0,0 @@
-<div class="tournamentNavigation">
-	<form action="registration/start.php" method="post">
-		<input type="hidden" name="id" value=<?=$tournamentID?>/>
-		<button type="submit">Почати реєстрацію</button>
-	</form>
-</div>
-
