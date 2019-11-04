6b02f9bf38b05eb1e84ea276e2959a138126e04d
labirinto <delevkoo@gmail.com>
2019-11-04 11:51:59 +0100

    fix img_upload bug

--- public/js/img_upload.js
+++ public/js/img_upload.js
@@ -1,14 +1,17 @@
-function readURL(input) {
+function readURL(input, class_id) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function(e) {
-            $('#imgPreview').css('background-image', 'url('+e.target.result +')');
-            $('#imgPreview').hide();
-            $('#imgPreview').fadeIn(650);
+            $(class_id).css('background-image', 'url('+e.target.result +')');
+            $(class_id).hide();
+            $(class_id).fadeIn(650);
         }
         reader.readAsDataURL(input.files[0]);
     }
 }
-$("#imgUpload").change(function() {
-    readURL(this);
+$("#playerImg_upload").change(function() {
+    readURL(this, '#playerImg_preview');
+});
+$("#clubImg_upload").change(function() {
+    readURL(this, '#clubImg_preview');
 });
