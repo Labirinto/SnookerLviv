8f7584c09f81dec3b33ab74012480d16bd8af9f8
labirinto <delevkoo@gmail.com>
2019-11-04 23:40:18 +0100

    add apology styles

--- templates/apology.php
+++ templates/apology.php
@@ -1,4 +1,20 @@
-<h1><?= htmlspecialchars($errorType) ?> </h1>
-<p><?= htmlspecialchars($message) ?></p>
 
-<a href="javascript:history.go(-1);">Повернутись</a>
+<link rel="stylesheet" type="text/css" href="<?=PATH_H?>css/error_page.css">
+		<div class="margin-b_30"></div>
+		<div class="error_page_box">
+            <div class="error_page_img">
+                <img src="<?=PATH_H?>img/sl_logo.png" alt="BilliardHub Logo">
+            </div>
+			<span class="error_page_header">
+				Помилка
+			</span>
+			<span class="error_page_text">
+				<?=htmlspecialchars($message)?>
+			</span>
+            <a href="javascript:history.go(-1);">
+				<span class="error_page_register">
+					Повернутись
+				</span>
+			</a>
+        </div>
+
